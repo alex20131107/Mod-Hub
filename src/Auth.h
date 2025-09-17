2a139645cd2cc195d1b5b1df7e6de77d78317915
alex20131107 <alhovikaleksej429@gmail.com>
2025-09-17 19:36:31 +0300

    Update Auth.h

--- src/Auth.h
+++ src/Auth.h
@@ -1,1 +1,28 @@
+#ifndef AUTH_H
+#define AUTH_H
 
+#include <string>
+#include <Wt/Auth/AuthService.h>
+#include <Wt/Auth/PasswordService.h>
+#include <Wt/Auth/PasswordVerifier.h>
+#include <Wt/Dbo/Session.h>
+#include <Wt/Dbo/backend/Sqlite3.h>
+
+class Auth {
+private:
+    Wt::Auth::AuthService authService;
+    Wt::Auth::PasswordService passwordService;
+    std::unique_ptr<Wt::Dbo::backend::Sqlite3> sqlite3;
+    Wt::Dbo::Session session;
+
+public:
+    Auth();
+    
+    Wt::Auth::AuthService& getAuthService() { return authService; }
+    Wt::Auth::PasswordService& getPasswordService() { return passwordService; }
+    Wt::Dbo::Session& getSession() { return session; }
+    
+    void configureAuth();
+};
+
+#endif // AUTH_H
